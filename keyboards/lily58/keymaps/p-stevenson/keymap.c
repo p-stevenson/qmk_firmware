cfec13afddc709b8d742cefd2a658edaad22f6fd
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-10-03 16:43:57 +0100

    Minor adjustments

--- keyboards/lily58/keymaps/p-stevenson/keymap.c
+++ keyboards/lily58/keymaps/p-stevenson/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_P7,    KC_P8,    KC_P9,   _______, KC_PMNS,\
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_P0,   KC_P4,    KC_P5,    KC_P6,   XXXXXXX, KC_GRV, \
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, XXXXXXX,      XXXXXXX, KC_PDOT, KC_P1,    KC_P2,    KC_P3,   XXXXXXX, KC_ENT, \
-                            _______, _______, _______, _______,      XXXXXXX, _______, _______, _______ \
+                            _______, _______, _______, _______,      XXXXXXX, _______, XXXXXXX, _______ \
 ),
 
 // --------------------------------------------------------------------------------
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX,  _______, XXXXXXX, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LGUI(KC_SLSH),XXXXXXX,KC_HOME, KC_END,  XXXXXXX, KC_PGUP,  KC_PGDN, XXXXXXX, \
-                             _______, XXXXXXX, _______, _______,      _______, _______, _______, _______ \
+                             _______, _______, _______, _______,      XXXXXXX, _______, XXXXXXX, _______ \
 ),
 // --------------------------------------------------------------------------------
 
