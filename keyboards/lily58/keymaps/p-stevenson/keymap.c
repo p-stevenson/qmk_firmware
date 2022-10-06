a9df474135e58fe8e6d6109e404e3c604c28c543
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-10-06 15:52:50 +0100

    Move pgdn and pgup left one key

--- keyboards/lily58/keymaps/p-stevenson/keymap.c
+++ keyboards/lily58/keymaps/p-stevenson/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX,  _______, XXXXXXX, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_TERM, KC_DUPE,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CMNT,      XXXXXXX,KC_HOME, KC_END,  XXXXXXX, KC_PGUP,  KC_PGDN, XXXXXXX, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CMNT,      XXXXXXX,KC_HOME, KC_END,  KC_PGUP, KC_PGDN,  XXXXXXX, XXXXXXX, \
                              _______, _______, _______, _______,      _______, _______, XXXXXXX, _______ \
 ),
 // --------------------------------------------------------------------------------
