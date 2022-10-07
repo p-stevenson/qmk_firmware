aee62b620394c640da7670c47a386f75b2a7bc31
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-10-07 14:59:06 +0100

    Add WS and OS Terminal Leaders

--- keyboards/lily58/keymaps/p-stevenson/keymap.c
+++ keyboards/lily58/keymaps/p-stevenson/keymap.c
@@ -3,22 +3,22 @@ enum {
   TD_CC,
   TD_BRCS,
   TD_SLSH,
-  TD_DEBUG,
+  TD_DBUG,
 };
 
 enum my_keycodes {
-    KC_TERM = LALT(KC_F12),
     KC_CMNT = LCTL(KC_SLSH),
     KC_DUPE = LCTL(KC_D),
-    KC_DEBUG = LSFT(KC_F9),
+    KC_DBUG = LSFT(KC_F9),
     KC_RERUN = LCTL(KC_F5),
+    KC_BRKP = LCTL(KC_F8),
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_CC] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_CAPS), //  Tap once for LCTRL, twice for CAPS
   [TD_BRCS] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC), //  Tap once for LEFT Braces, twice for RIGHT
   [TD_SLSH] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_BSLS), // Tap once for BACK Slash, twice for FORWARD
-  [TD_DEBUG] = ACTION_TAP_DANCE_DOUBLE(KC_DEBUG, KC_RERUN), // Tap once for Debug, twice for rerun debugger
+  [TD_DBUG] = ACTION_TAP_DANCE_DOUBLE(KC_DBUG, KC_RERUN), // Tap once for Debug, twice for rerun debugger
 };
 
 #ifdef PROTOCOL_LUFA
@@ -60,8 +60,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX,  _______, XXXXXXX, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_TERM, KC_DUPE,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TD(TD_DEBUG), KC_CMNT,     XXXXXXX,KC_HOME, KC_END,  KC_PGUP, KC_PGDN,  XXXXXXX, XXXXXXX, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DUPE,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, \
+  _______, KC_BRKP, XXXXXXX, XXXXXXX, XXXXXXX, TD(TD_DBUG), KC_CMNT,     XXXXXXX,KC_HOME, KC_END,  KC_PGUP, KC_PGDN,  XXXXXXX, XXXXXXX, \
                               _______, _______, _______, _______,     _______, _______, XXXXXXX, _______ \
 ),
 // --------------------------------------------------------------------------------
@@ -91,5 +91,19 @@ void matrix_scan_user(void) {
     SEQ_TWO_KEYS(KC_O, KC_O) {
       SEND_STRING(";");
     }
+// WEBSTORM TERMINAL
+    SEQ_TWO_KEYS(KC_T, KC_W) {
+    register_code(KC_LALT);
+    register_code(KC_F12);
+    unregister_code(KC_LALT);
+    unregister_code(KC_F12);
   }
+// OS TERMINAL
+    SEQ_TWO_KEYS(KC_T, KC_X) {
+    register_code(KC_LGUI);
+    register_code(KC_ENT);
+    unregister_code(KC_LGUI);
+    unregister_code(KC_ENT);
+    }
+}
 }
